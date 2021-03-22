15f551e3127dbf1134e5aeced9d855220906d280
gmu1026 <curiouscomponent@gmail.com>
2021-03-22 16:18:21 +0900

    fix: deployment script - error

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "START DEPLOYING: $PROJECT_NAME"
 
 echo "CHECK RUNNING THIS APP..."
 
-RUNNING_APP_PID=$(pgrep -fl test | grep jar | awk '{print $1}')
+RUNNING_APP_PID=$(pgrep -fl test | awk '{print $1}')
 
 if [ -z "$RUNNING_APP_PID"]; then
   echo "NOT RUNNING DIARY APP"
@@ -17,7 +17,5 @@ else
 fi
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-
-chmod+x $JAR_NAME
 
 nohup java -jar $JAR_NAME > $REPOSITORY/log/nohup.out 2>&1 &
